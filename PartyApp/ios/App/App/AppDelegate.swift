25acd6d943e68c9f7b2fd07843b889bcc92e030e
BrunoCecco <bruno.ceccolini@gmail.com>
2021-04-22 23:18:18 +0100

    Revert "update some ios files added"
    This reverts commit ee4cdee836d30288e7ffd0846ecb7f3af611077f.

--- PartyApp/ios/App/App/AppDelegate.swift
+++ PartyApp/ios/App/App/AppDelegate.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Firebase
 import Capacitor
 
 @UIApplicationMain
@@ -10,7 +9,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
-    FirebaseApp.configure()
     return true
   }
 
